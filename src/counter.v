e81e5ade7dcffba27dc27cae4ce0ddc83846f93e
Emilian Miron <emilian@slim>
2023-03-19 19:53:03 -0400

    Add some cells to look at GDS.

--- src/counter.v
+++ src/counter.v
@@ -1,55 +1,63 @@
 `default_nettype none
 
-
 module diferential_muxpga (
   input [7:0] io_in,
   output [7:0] io_out
 );
-    seven_segment_seconds s(io_in, io_out);
+
+   wire        clk = io_in[0];
+   wire        reset = io_in[1];
+   wire [4:0]  cfg = io_in[7:3];
+
+   localparam  ROWS = 8;
+   localparam  COLS = 8;
+
+   generate
+      genvar   row;
+      genvar   col;
+
+      wire [0:ROWS-1] cell_q[0:COLS-1];
+      for (row = 0; row < ROWS; row = row + 1'b1) begin
+         for (col = 0; col < COLS; col = col + 1'b1) begin
+            wire left;
+            assign left = col > 0 ? cell_q[row][col - 1] : io_in[row];
+            diferential_cell c(clk, reset, {io_in[2], left}, cfg, cell_q[row][col]);
+         end
+
+         assign io_out[row] = cell_q[row][COLS - 1'b1];
+      end
+   endgenerate
+
 endmodule
 
-module seven_segment_seconds #( parameter MAX_COUNT = 1000 ) (
-  input [7:0] io_in,
-  output [7:0] io_out
-);
-    
-    wire clk = io_in[0];
-    wire reset = io_in[1];
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
+module diferential_cell
+  (
+    input       clk,
+    input       reset,
+    input [1:0] in,
+    input [4:0] cfg,
+    output      q
+    );
 
-    // slow clock out on the last gpio
-    assign io_out[7] = second_counter[4];
+   reg          dff;
+   reg         f_out;
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
+   always @(*) begin
+      case(in)
+        0:  f_out = cfg[0];
+        1:  f_out = cfg[1];
+        2:  f_out = cfg[2];
+        3:  f_out = cfg[3];
+      endcase
+   end
 
-    always @(posedge clk) begin
-        // if reset, set counter to 0
-        if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
-        end else begin
-            // if up to 16e6
-            if (second_counter == MAX_COUNT) begin
-                // reset
-                second_counter <= 0;
+   always @(posedge clk) begin
+      if (reset) begin
+         dff <= 0;
+      end else begin
+         dff <= f_out;
+      end
+   end
 
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
-        end
-    end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
-
+   assign q = cfg[4] ? dff : f_out;
 endmodule
