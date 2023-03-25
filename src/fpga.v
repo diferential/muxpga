2d621d42763dc0288a64a46ffe3c5a0c82bb2b07
Emilian Miron <emilian@slim>
2023-03-25 19:58:21 -0400

    Add inputs to first row.

--- src/fpga.v
+++ src/fpga.v
@@ -1,5 +1,9 @@
 `default_nettype none
 
+// TODO(emilian): add cfg scan chain
+// TODO(emilian): pass in inputs
+// TODO(emilian): add some preliminary tests
+
 module diferential_muxpga (
   input [7:0] io_in,
   output [7:0] io_out
@@ -7,59 +11,67 @@ module diferential_muxpga (
 
    wire        clk = io_in[0];
    wire        reset = io_in[1];
-   wire [3:0]  cfg = io_in[6:3];
+   wire [3:0]  cfg_mux = {io_in[7:6], io_in[7:6]};
+   wire [3:0]  cfg = 4'b0;
 
-   localparam  ROWS = 6;
-   localparam  COLS = 6;
-   localparam  BITS = 4;
+   localparam  ROWS = 5;
+   localparam  COLS = 3;
+
+   // input/output register bits
+   localparam  CELL_BITS = 4;
+   // function value configuration bits.
+   localparam  CFG_BITS = 4;
+   // each input gets a routing dff with these many bits.
+   localparam  INPUT_MUX_BITS = 2;
+   localparam  BOTH_MUX_BITS = 4;
+
+   wire [CELL_BITS-1:0] cell_q[0:ROWS-1][0:COLS-1];
 
    generate
       genvar   row;
       genvar   col;
 
-      wire [BITS-1:0] cell_q[0:COLS-1][0:ROWS-1] ;
       for (row = 0; row < ROWS; row = row + 1'b1) begin
          for (col = 0; col < COLS; col = col + 1'b1) begin
-            wire [3:0] cfg_mux;
-            assign cfg_mux = cfg;
-            
-            reg [BITS-1:0] cell_in1;
-            reg [BITS-1:0] cell_in2;
+            if (row != 0) begin
+               wire [BOTH_MUX_BITS-1:0] mux_bits = cfg_mux;
 
-            localparam rminus1 = (ROWS + row - 1) % ROWS;
-            localparam rplus1 = (ROWS + row + 1) % ROWS;
-            localparam cminus1 = (COLS + col - 1) % COLS;
-            localparam cplus1 = (COLS + col + 1) % COLS;
+               reg [CELL_BITS-1:0]      cell_in1;
+               reg [CELL_BITS-1:0]      cell_in2;
 
-            always @(*) begin
-               case(cfg_mux[1:0])
-                 0:  cell_in1 = cell_q[rminus1][col];
-                 1:  cell_in1 = cell_q[rminus1][cminus1];
-                 2:  cell_in1 = cell_q[row][cminus1];
-                 3:  cell_in1 = cell_q[row][cplus1];
-                 default:  cell_in1 = cell_q[row][col];
-               endcase
+               localparam               rminus1 = (ROWS + row - 1) % ROWS;
+               localparam               rplus1 = (ROWS + row + 1) % ROWS;
+               localparam               cminus1 = (COLS + col - 1) % COLS;
+               localparam               cplus1 = (COLS + col + 1) % COLS;
+
+               always @(*) begin
+                  case(mux_bits[INPUT_MUX_BITS-1:0])
+                    0:  cell_in1 = cell_q[rminus1][col];
+                    1:  cell_in1 = cell_q[rminus1][cminus1];
+                    2:  cell_in1 = cell_q[row][cminus1];
+                    3:  cell_in1 = cell_q[row][cplus1];
+                    default:  cell_in1 = cell_q[row][col];
+                  endcase
+               end
+               
+               always @(*) begin
+                  case(mux_bits[BOTH_MUX_BITS-1:INPUT_MUX_BITS])
+                    0:  cell_in2 = cell_q[rminus1][col];
+                    1:  cell_in2 = cell_q[rminus1][cminus1];
+                    2:  cell_in2 = cell_q[row][cminus1];
+                    3:  cell_in2 = cell_q[row][cplus1];
+                    default:  cell_in2 = cell_q[row][col];
+                  endcase
+               end
+               diferential_cell c(clk, reset, cell_in1, cell_in2, cfg, cell_q[row][col]);
+            end else begin
+               assign cell_q[row][col] = io_in[5:2];
             end
-
-            always @(*) begin
-               case(cfg_mux[3:2])
-                 0:  cell_in2 = cell_q[rminus1][col];
-                 1:  cell_in2 = cell_q[rminus1][cminus1];
-                 2:  cell_in2 = cell_q[row][cminus1];
-                 3:  cell_in2 = cell_q[row][cplus1];
-                 default:  cell_in2 = cell_q[row][col];
-               endcase
-            end
-
-            diferential_cell c(clk, reset, cell_in1, cell_in2, cfg, cell_q[row][col]);
          end
-
-         assign io_out[row] = cell_q[row][COLS - 1'b1];
       end
    endgenerate
 
    assign io_out = {cell_q[ROWS - 1][0], cell_q[ROWS - 1][COLS - 1]};
-
 endmodule
 
 module diferential_cell
@@ -80,8 +92,8 @@ module diferential_cell
    
    always @(*) begin
       case(cfg[1:0])
-        0:  f_out = in1 & in2;
-        1:  f_out = in1 | in2;
+        0:  f_out = in1 | in2;
+        1:  f_out = in1 & in2;
         2:  f_out = in1;
         3:  f_out = in2;
       endcase
