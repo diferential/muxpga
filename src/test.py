88b3838ab19fe778538591388f1d8a953cd04d8b
Emilian Miron <emilian@slim>
2023-04-01 07:31:42 -0400

    Add sv2v build rule for fpga.sv

--- src/test.py
+++ src/test.py
@@ -2,37 +2,48 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+CMD_SHIFT_CFG = 0;
+CMD_RUN = 1;
+ROWS = 5;
+COLS = 3;
+
+async def do_input_cycle(dut, rst, io_in, cmd):
+    await Timer(1, units='us')
+
+    dut.rst.value = rst;
+    dut.io_in.value = io_in;
+    dut.cmd.value = cmd;
+
+    dut._log.info("sending rst={} in={} cmd={}".format(rst, io_in, cmd));
+    await Timer(1, units='us');
+    
+    dut.rst.value = 1;
+    await ClockCycles(dut.clk, 10);
+
+    val = dut.segments.value;
+    dut._log.info("clock out {}".format(val));
+    return val;
+
+async def do_reset(dut):
+    for i in range(10):
+        # write some random cmd values, they should be ignored.
+        assert(do_input_cycle(dut, 1, 3, CMD_RUN) == 0);
+
 @cocotb.test()
 async def test_cfg0_isdff(dut):
-    dut.rst.value = 1
-    dut.io_in.value = 2;
-    dut.cmd.value = 0;
-
     dut._log.info("start")
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
     dut._log.info("reset")
-    dut.rst.value = 1
-    await ClockCycles(dut.clk, 10)
-    dut.rst.value = 0
+    await do_reset(dut);
 
+    cells = (ROWS-1)*COLS;
+
+    cmd = 0;
     dut._log.info("dff test")
-    for i in range(10):
-        dut._log.info("pushing {}".format(i % 4))
-
-        dut.io_in.value = (i % 4);
-        await ClockCycles(dut.clk, 1)
-        dut._log.info("clock out {}".format(dut.segments.value))
-
-        await ClockCycles(dut.clk, 1)
-        dut._log.info("clock out {}".format(dut.segments.value))
-
-        await ClockCycles(dut.clk, 1)
-        dut._log.info("clock out {}".format(dut.segments.value))
-
-ROWS = 5;
-COLS = 3;
+    for i in range(cells):
+        do_input_cycle(0, i%4, cmd);
 
 # TODO(emilian): Make this work.
 @cocotb.test()
